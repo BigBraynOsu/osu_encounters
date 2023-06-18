275474bd263b267ac105a75d9bb10ccfac302ab4
BigBraynOsu <newtimser@gmail.com>
2023-06-18 23:58:16 +0200

    Hinzufügen Frontend SvelteKit

---
+++ OsuEncounter/Frontend/my-app/src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<div class="header">
+    <h1>Osu!Encounter</h1> 
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Osu%21_Logo_2016.svg/2048px-Osu%21_Logo_2016.svg.png">
+</div>
+
+<div>
+    <playercard/>
+</div>
+
+
+<style>
+
+.header {
+  padding: 15px;
+  text-align: left;
+  background: #1abc9c;
+  color: white;
+  font-size: 30px;
+  margin: 0px;
+  position: relative;
+}
+
+img{
+    height: 10rem;
+    width: auto;
+    float: right;
+}
+
+
+
+
+</style>
